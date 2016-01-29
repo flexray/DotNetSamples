5ae6d49c2f739773c9a0af6321c6f11a71616eed
Maciej Stopa <mastopa@microsoft.com>
2016-01-29 13:24:09 +0100

    NewSolution

---
+++ WebAppsDemo/WebAppsDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppsDemo.Startup))]
+namespace WebAppsDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
